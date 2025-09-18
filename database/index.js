5a0b461d91e6ec019ac7f2b9f22db04096cb30b7
Franccesco Petta <franccescopettamuller@gmail.com>
2025-09-18 18:23:02 -0400

    datatable sync

--- database/index.js
+++ database/index.js
@@ -3,6 +3,17 @@ const User = require("./user");
 const Event = require("./events");
 const Member = require("./members");
 
+const syncDatabase = async () => {
+  try {
+    await Member.sync({ alter: true });
+    console.log('Members table synced successfully');
+  } catch (error) {
+    console.error('Error syncing members table:', error);
+  }
+};
+
+syncDatabase();
+
 module.exports = {
   db,
   User,
